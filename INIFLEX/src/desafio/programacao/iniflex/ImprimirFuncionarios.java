0cfd7323fa06a1c9b84e76a334370ec7507fd813
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-31 22:46:31 -0300

    Atualização de formato de numeração

--- INIFLEX/src/desafio/programacao/iniflex/ImprimirFuncionarios.java
+++ INIFLEX/src/desafio/programacao/iniflex/ImprimirFuncionarios.java
@@ -1,7 +1,6 @@
 package desafio.programacao.iniflex;
 
-// import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.text.NumberFormat;
 import java.util.List;
 
 import desafio.programacao.interfac.Funcionario;
@@ -9,12 +8,16 @@ import desafio.programacao.interfac.Funcionario;
 public class ImprimirFuncionarios {
     public static void imprimir(List<Funcionario> funcionarios) {
         System.out.println("Lista de Funcionários:");
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        numberFormat.setMinimumFractionDigits(2);
+        numberFormat.setMaximumFractionDigits(2);
+        numberFormat.setGroupingUsed(true);
+
         for (Funcionario funcionario : funcionarios) {
-            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            
             System.out.println("Nome: " + funcionario.nome);
-            System.out.println("Data de Nascimento: " + funcionario.dataNascimento.format(dateFormatter));
-            System.out.println("Salário: " + funcionario.salario);
+            System.out.println("Data de Nascimento: " + funcionario.dataNascimento);
+            System.out.println("Salário: " + numberFormat.format(funcionario.salario));
             System.out.println("Função: " + funcionario.funcao);
             System.out.println();
         }
