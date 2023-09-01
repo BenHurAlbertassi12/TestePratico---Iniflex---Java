5e76ce72109dd67620dec6ffeac192cdf123bd41
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-31 21:45:00 -0300

    Fundionarios

--- INIFLEX/src/desafio/programacao/iniflex/ImprimirFuncionarios.java
+++ INIFLEX/src/desafio/programacao/iniflex/ImprimirFuncionarios.java
@@ -1,5 +1,7 @@
 package desafio.programacao.iniflex;
 
+// import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import desafio.programacao.interfac.Funcionario;
@@ -8,12 +10,13 @@ public class ImprimirFuncionarios {
     public static void imprimir(List<Funcionario> funcionarios) {
         System.out.println("Lista de Funcionários:");
         for (Funcionario funcionario : funcionarios) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            
             System.out.println("Nome: " + funcionario.nome);
-            System.out.println("Data de Nascimento: " + funcionario.dataNascimento);
+            System.out.println("Data de Nascimento: " + funcionario.dataNascimento.format(dateFormatter));
             System.out.println("Salário: " + funcionario.salario);
             System.out.println("Função: " + funcionario.funcao);
             System.out.println();
         }
     }
 }
-
